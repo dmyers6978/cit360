2eacad10d6b9114ab53812928975df31de63c0a6
Dustin Myers <36669070+dmyers6978@users.noreply.github.com>
2020-03-24 20:26:58 -0600

    Add files via upload

---
+++ servlet/src/CIT360/Main.java
@@ -0,0 +1,14 @@
+package CIT360;
+
+import javax.servlet.http.HttpServlet;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        HttpServlet helloWorld  = new HelloWorld();
+        System.out.println(helloWorld.toString());
+
+
+    }
+}
